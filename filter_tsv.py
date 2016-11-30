84dd252359f5767f8682ed90a2e0652c2a5a843c
mancini pierre <pierremncini@gmail.com>
2016-11-30 16:45:27 +0100

    Les lignes contenant uniquement des tabulation ne sont pas lues

--- filter_tsv.py
+++ filter_tsv.py
@@ -48,26 +48,7 @@ def compare_clinsig_voc(list_clinsig, voc):
 
 
 def apply_filter(line):
-    """ Apply filter on csv row given in first argument.
-
-    Conditions:
-    1ere combinaison:
-    Pos coverage >200
-    ET
-    Var freq >2 (%)
-    ET
-    1000G_ALL_AF <0.01
-    ET
-    Clinvar: Clinsig=Pathogenic OU Drug response OU Likely pathogenic
-
-    2eme combinaison:
-    Pos coverage >200
-    ET
-    Var freq >5 (%)
-    ET
-    1000G_ALL_AF <0.01
-    ET
-    Clinvar: Clinsig= Uncertain significance OU Conflicting interpretations of pathogenicity OU Unknown OU NA"""
+    """ Apply filter on csv row given in first argument. """
 
     valid = True
 
@@ -76,29 +57,15 @@ def apply_filter(line):
     # Conditions communes
     if (int(line['Pos.Cov.']) < 200) | (float_csv(line['1000G_ALL_AF']) > 0.01):
         valid = False
-        # print('Pos.Cov et 1000G_ALL stop')
-    # else:
-        # print('Pos.Cov et 1000G_ALL ok')
 
     # 1er cas
-
     if int(line['Var.freq.']) < 2:
         valid = False
-    # print('Var freq ok')
 
     clinvar_ok = compare_clinsig_voc(list_clinsig, ['pathogenic', 'drug-response', 'likely-pathogenic'])
-    """ clinvar_ok = False
-    for w in ['pathogenic', 'drug-response', 'likely-pathogenic']:
-        if w in list_clinsig:
-            clinvar_ok = True
-            print("{w} in {line}".format(w=w, line=list_clinsig))
-            print('')
-            break """
 
     if not clinvar_ok:
         valid = clinvar_ok
-
-    # print('1er cas: {v}'.format(v=valid))
 
     # 2em cas
 
@@ -109,20 +76,10 @@ def apply_filter(line):
 
         clinvar_ok = compare_clinsig_voc(list_clinsig, ['uncertain-significance',
             'conflicting-interpretations-of-pathogenicity', 'unknown', 'NA', ''])
-        """ clinvar_ok = False
-        for w in ['uncertain-significance', 'conflicting-interpretations-of-pathogenicity',
-                'unknown', 'NA', '']:s
-            if w in list_clinsig:
-                clinvar_ok = True
-                print("{w} in {line}".format(w=w, line=list_clinsig))
-                print('')
-                break """
 
         if not clinvar_ok:
             valid = clinvar_ok
 
-    # print('valid: {v}'.format(v=valid))
-    # print('~~~~~~~~~~~~~~')
     return valid
 
 
@@ -137,19 +94,19 @@ if __name__ == '__main__':
 
     liste_file_name = os.listdir(in_dir)
 
-    csv_dict = []
-
     for file_name in liste_file_name:
         iter = re.finditer(r"annotation.*\.tsv$", file_name)
         for match in iter:
             if match is not None:
+                csv_dict = []
                 with open(os.path.join(in_dir, file_name), 'rb') as csvfile:
-                    # print(file_name)
-                    dict_reader, dict_reader_temp = itertools.tee(csv.DictReader(csvfile, delimiter='\t'))
-                    header = dict_reader_temp.next().keys()
+                    dict_reader, temp_dict_reader = itertools.tee(csv.DictReader(csvfile, delimiter='\t'))
+                    header = next(temp_dict_reader).keys()
                     for line in dict_reader:
-                        if apply_filter(line):
-                            csv_dict.append(line)
+                        # pas de ligne avec uniquement des \t
+                        if ''.join(line.values()):
+                            if apply_filter(line):
+                                csv_dict.append(line)
                 with open(os.path.join(out_dir, file_name), 'wb') as f:
                     w = csv.DictWriter(f, delimiter='\t', fieldnames=header)
                     w.writeheader()
