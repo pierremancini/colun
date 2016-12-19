ecd4431ec6008e9b2635bef4ece21585f351025a
Pierre Mancini <pierremncini@gmail.com>
2016-12-19 11:29:16 +0100

    écriture des meta files

--- build_study_data.py
+++ build_study_data.py
@@ -43,14 +43,26 @@ if __name__ == '__main__':
     """
     out_dir = 'cancer_study_data'
 
-    nom_dir_colon = 'colon'
-    nom_dir_lung = 'lung'
-
     case_list_name = 'nom de case liste'
     case_list_description = 'description de case liste'
+    case_list_ids = []
+
+    # meta_study.txt
+    name_meta_study = ''
+    description_meta_study = ''
+    short_name_meta_study = 'colung'
+    add_global_case_list = ''
+
+    # meta_mutations_extended.txt
+    maf_filename = 'mutations.maf'
+    profile_description = 'Mutation description truc bidul'
 
     if not os.path.exists(out_dir):
         os.mkdir(out_dir)
+    if not os.path.exists(os.path.join(out_dir, 'colon_study')):
+        os.mkdir(os.path.join(out_dir, 'colon_study'))
+    if not os.path.exists(os.path.join(out_dir, 'lung_study')):
+        os.mkdir(os.path.join(out_dir, 'lung_study'))
 
     # Clinical data . Patients
     # Regroupe tout les patients de l'étude
@@ -86,32 +98,69 @@ if __name__ == '__main__':
     #  patients -> dict_samples
     for study_type in dict_samples:
         if study_type == 'colon':
-
+            study_dir = 'colon_study'
         elif study_type == 'lung':
+            study_dir = 'lung_study'
 
+        if not os.path.exists(os.path.join(out_dir, study_dir, 'case_lists')):
+            os.mkdir(os.path.join(out_dir, study_dir, 'case_lists'))
 
-        with open(os.path.join(out_dir, , 'data_patients.txt'), 'wb') as fpatients, open(os.path.join(out_dir, 'data_samples.txt'), 'wb') as fsamples, open(os.path.join(out_dir, "cases_custom.txt")) as fcases:
-            # écriture data_patients.txt
-            en_tete = "#Patient Identifier\n#Patient identifier\n#STRING\n#1PATIENT_ID\n"
+        # ~~~~ Partie data ~~~~
+        with open(os.path.join(out_dir, study_dir, 'data_patients.txt'), 'wb') as fpatients, open(os.path.join(out_dir, study_dir, 'data_samples.txt'), 'wb') as fsamples, open(os.path.join(out_dir, study_dir, 'case_lists', "cases_custom.txt"), 'wb') as fcases:
+
+            en_tete = "#Patient Identifier\n#Patient identifier\n#STRING\n#1\nPATIENT_ID\n"
             fpatients.write(en_tete)
-            fpatients.write(id_patient + "\n")
 
-            # Clinical data . Samples
-            # Regroupe tout les samples des patients de l'étude
-            en_tete = "#Patient Identifier\tSample Identifier\n#Patient Identifier\tSample Identifier\n#STRING\tSTRING\n#1\t1\n"
-
-            # écriture data_samples.txt
+            en_tete = "#Patient Identifier\tSample Identifier\n#Patient Identifier\tSample Identifier\n#STRING\tSTRING\n#1\t1\nPATIENT_ID\tSAMPLE_ID\n"
             fsamples.write(en_tete)
-            for patient_id in patients:
-                for sample in patients[patient_id]:
-                    fsamples.write(patient_id + "\t" + sample['sample_id'] + "\n")
 
-            # écriture des fichiers cases_lists
-            if not os.path.exists(os.path.join(out_dir, 'case_lists')):
-                os.path.exists(os.path.join(out_dir, 'case_lists'))
-                fcases.write("cancer_study_identifcasesier: " + study_name + "\n")
-                fcases.write("stable_id: " + study_name + "_custom\n")
-                fcases.write("case_list_name: "+ case_list_name + "\n")
-                fcases.write("case_list_description: " + case_list_description + "\n")
-                fcases.write("case_list_ids: ")
-                fcases.write("\t".join(case_list_ids))
+            for patient_id in dict_samples[study_type]:
+
+                fpatients.write(patient_id + "\n")
+                case_list_ids.append(patient_id)
+
+                for sample_id in dict_samples[study_type][patient_id]:
+                    fsamples.write(patient_id + "\t" + sample_id + "\n")
+
+            fcases.write("cancer_study_identifcasesier: " + study_type + "\n")
+            fcases.write("stable_id: " + study_type + "_custom\n")
+            fcases.write("case_list_name: " + case_list_name + "\n")
+            fcases.write("case_list_description: " + case_list_description + "\n")
+            fcases.write("case_list_ids: ")
+            fcases.write("\t".join(case_list_ids))
+
+        # ~~~~ Partie meta ~~~~
+
+        with open(os.path.join(out_dir, study_dir, 'meta_study.txt'), 'wb') as f:
+            if study_type == 'colon':
+                f.write('type_of_cancer: coadread\n')
+            elif study_type == 'lung':
+                f.write('type_of_cancer: nsclc\n')
+            f.write('cancer_study_identifier:' + study_dir + '\n')
+            f.write('name: ' + name_meta_study + '\n')
+            f.write('description: ' + description_meta_study + '\n')
+            f.write('short_name: ' + short_name_meta_study + '\n')
+            f.write('add_global_case_list: true\n')
+
+        # meta clinical
+        with open(os.path.join(out_dir, study_dir, 'meta_samples.txt'), 'wb') as f:
+            f.write('cancer_study_identifier:' + study_dir + '\n')
+            f.write('genetic_alteration_type: CLINICAL\n')
+            f.write('datatype: SAMPLE_ATTRIBUTES\n')
+            f.write('data_filename: data_samples.txt\n')
+
+        with open(os.path.join(out_dir, study_dir, 'meta_patients.txt'), 'wb') as f:
+            f.write('cancer_study_identifier:' + study_dir + '\n')
+            f.write('genetic_alteration_type: CLINICAL\n')
+            f.write('datatype: PATIENT_ATTRIBUTES\n')
+            f.write('data_filename: data_patients.txt\n')
+
+        with open(os.path.join(out_dir, study_dir, 'meta_mutations_extended.txt'), 'wb') as f:
+            f.write('cancer_study_identifier:' + study_dir + '\n')
+            f.write('genetic_alteration_type: MUTATION_EXTENDED\n')
+            f.write('datatype: MAF\n')
+            f.write('show_profile_in_analysis_tab: true\n')
+            f.write('profile_name: Mutations\n')
+            f.write('profile_description: ' + profile_description + '\n')
+            f.write('data_filename: ' + maf_filename + '\n')
+            f.write('swissprot_identifier: name\n')
